ac4edb1b69793fe80a95d06a65134216b01f0df4
Kaustubh lawale <kaustubhlawale@gmail.com>
2022-10-02 02:45:32 +0530

    model

---
+++ Assign1/src/Model/Employee.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.io.File;
+
+/**
+ *
+ * @author kaustubh
+ */
+public class Employee {
+    private String name;
+    private int number;
+    private String email;
+    private int employee_ID;
+    private int age;
+    private String gender;
+    private int startDate;
+    private String level;
+    private String team;
+    private File mProfileLocation;
+    private String positionTitle;
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEmployee_ID() {
+        return employee_ID;
+    }
+
+    public void setEmployee_ID(int employee_ID) {
+        this.employee_ID = employee_ID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(int startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public File getmProfileLocation() {
+        return mProfileLocation;
+    }
+
+    public void setmProfileLocation(File mProfileLocation) {
+        this.mProfileLocation = mProfileLocation;
+    }
+}

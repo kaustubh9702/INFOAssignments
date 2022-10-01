017b8983c4ebcd07a7dd3509a18cc316d65fff56
Kaustubh lawale <kaustubhlawale@gmail.com>
2022-10-02 04:04:55 +0530

    jpanel

---
+++ Assign1/src/Model/EmployeeHistory.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kaustubh
+ */
+public class EmployeeHistory {
+    
+    private ArrayList<Employee> history;
+    
+    public EmployeeHistory(){
+        this.history = new ArrayList<Employee>();
+    }
+
+    public ArrayList<Employee> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Employee> history) {
+        this.history = history;
+    }
+    
+    
+}

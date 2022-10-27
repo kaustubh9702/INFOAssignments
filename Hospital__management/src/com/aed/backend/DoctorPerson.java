8f62ede9b92b52ab54e8f100e2a27b7845834f3e
Kaustubh lawale <kaustubhlawale@gmail.com>
2022-10-26 23:15:06 -0400

    Patient doctor operations done!

---
+++ Hospital__management/src/com/aed/backend/DoctorPerson.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aed.backend;
+
+
+public class DoctorPerson extends Person{
+    private String mdeicalLicenseNumber;
+
+    public String getMdeicalLicenseNumber() {
+        return mdeicalLicenseNumber;
+    }
+
+    public void setMdeicalLicenseNumber(String mdeicalLicenseNumber) {
+        this.mdeicalLicenseNumber = mdeicalLicenseNumber;
+    }
+}
---
+++ Hospital__management/src/com/aed/backend/System.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aed.backend;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tarush
+ */
+public class System {
+    private ArrayList<Person> patientDir = new ArrayList<>();
+    private ArrayList<DoctorPerson> doctorDir = new ArrayList<>();
+    
+    
+}
